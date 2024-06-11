70e32cf0e4449c04d66e4c8562f4cc9a9409e654
Deyson Venté <deiandresvent@hotmail.com>
2024-06-11 12:53:16 -0500

    archivo de barriles para paginas de villains: usar lazyload.

---
+++ src/app/Villains/pages/index.ts
@@ -0,0 +1,3 @@
+export { DetailsComponent as VillainsDetails } from './details/details.component';
+export { ListComponent as VillainsList } from './list/list.component';
+export { VillainsHomeComponent as VillainsHome } from './villains-home/villains-home.component';
