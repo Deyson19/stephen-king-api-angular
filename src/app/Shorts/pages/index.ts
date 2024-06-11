aecb034b806666f9c23bcdb92749394d17d4eb6b
Deyson Venté <deiandresvent@hotmail.com>
2024-06-11 12:51:47 -0500

    archivo de barril para las paginas de shorts: no se va a usar, en su lugar implementar lazyload.

---
+++ src/app/Shorts/pages/index.ts
@@ -0,0 +1,3 @@
+export { DetailsComponent as ShortsDetails } from './details/details.component';
+export { ListComponent as ShortsList } from './list/list.component';
+export { ShortsHomeComponent as ShortsHome } from './shorts-home/shorts-home.component';
