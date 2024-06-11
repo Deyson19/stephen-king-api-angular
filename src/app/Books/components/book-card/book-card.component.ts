1df4fd88b583945d7584a71b666fe399f3e51f7b
Deyson Venté <deiandresvent@hotmail.com>
2024-06-11 15:44:32 -0500

    Card de libro/book implementada.

--- src/app/Books/components/book-card/book-card.component.ts
+++ src/app/Books/components/book-card/book-card.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { IBooks } from '../../../Interfaces/Books';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-book-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule,
+  ],
   templateUrl: './book-card.component.html',
-  styleUrl: './book-card.component.css'
+  styleUrl: './book-card.component.css',
 })
 export class BookCardComponent {
-
+  book = input.required<IBooks>();
 }
