bfbbd58c878336ca8357efe14cd8c8753003b517
Deyson Venté <deiandresvent@hotmail.com>
2024-06-11 12:49:36 -0500

    Componente para iterar la lista de Shorts

---
+++ src/app/Shorts/components/short-card/short-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-short-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './short-card.component.html'
+})
+export class ShortCardComponent {
+
+}
