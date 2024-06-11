a25ef1902c4f2f4450b25fb9984b73842a8ca6a7
Deyson Venté <deiandresvent@hotmail.com>
2024-06-11 12:47:17 -0500

    Vista/página para ver los detalles de un libro/books.

---
+++ src/app/Books/pages/details/details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './details.component.html',
+})
+export default class DetailsComponent {}
