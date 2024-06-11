179c2313121de7926e9f4130bc893131927c8703
Deyson Venté <deiandresvent@hotmail.com>
2024-06-11 12:52:15 -0500

    Componente para iterar la lista de villanos/villains

---
+++ src/app/Villains/components/villain-card/villain-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-villain-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './villain-card.component.html'
+})
+export class VillainCardComponent {
+
+}
