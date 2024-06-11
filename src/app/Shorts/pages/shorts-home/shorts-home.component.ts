821d50a46629f0f1394297eb5b4447b44aeba7d4
Deyson Venté <deiandresvent@hotmail.com>
2024-06-11 12:50:57 -0500

    Página principal de shorts

---
+++ src/app/Shorts/pages/shorts-home/shorts-home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './shorts-home.component.html',
+  styleUrl: './shorts-home.component.css',
+})
+export class ShortsHomeComponent {}
