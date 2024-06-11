0768afd0912949a3168203ccbbb921f4887fc11f
Deyson Venté <deiandresvent@hotmail.com>
2024-06-11 12:49:10 -0500

    Enum con las urls para hacer las peticiones.

---
+++ src/app/Shared/BaseUrl.ts
@@ -0,0 +1,8 @@
+export enum BaseUrl {
+  short = 'https://stephen-king-api.onrender.com/api/short',
+  shorts = 'https://stephen-king-api.onrender.com/api/shorts',
+  books = 'https://stephen-king-api.onrender.com/api/books',
+  book = 'https://stephen-king-api.onrender.com/api/book',
+  villans = 'https://stephen-king-api.onrender.com/api/villains',
+  villan = 'https://stephen-king-api.onrender.com/api/villain',
+}
