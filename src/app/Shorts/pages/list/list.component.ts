474dc62aea7bc8b28f6577a8508745885d0ec9d1
Deyson Venté <deiandresvent@hotmail.com>
2024-06-15 22:00:52 -0500

    Listado de elementos: Shorts

--- src/app/Shorts/pages/list/list.component.ts
+++ src/app/Shorts/pages/list/list.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShortsService } from '@services/shorts.service';
+import { SpinnerComponent } from '@shared/spinner/spinner.component';
+import { IShorts } from '@interfaces';
+import { tap } from 'rxjs';
+import { ShortCardComponent } from '../../components/short-card/short-card.component';
 @Component({
   standalone: true,
-  imports: [],
+  imports: [SpinnerComponent, ShortCardComponent, CommonModule],
   templateUrl: './list.component.html',
 })
-export class ListComponent {}
+export class ListComponent implements OnInit {
+  private _shortsService = inject(ShortsService);
+  public mostrarSpinner = true;
+  mensaje = 'Cargando el listado de Shorts';
+  shortsList: IShorts[] = [];
+
+  ngOnInit(): void {
+    this._shortsService
+      .getAll()
+      .pipe(tap(() => (this.mostrarSpinner = false)))
+      .subscribe((shorts) => {
+        this.shortsList = shorts.data;
+      });
+  }
+  displayedColumns: string[] = [
+    'id',
+    'title',
+    'type',
+    'year',
+    'collectedIn',
+    'created_at',
+  ];
+}
