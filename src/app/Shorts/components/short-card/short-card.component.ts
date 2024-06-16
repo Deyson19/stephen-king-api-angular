6331bd4d2829595d3b2a62392da27553cdb7c730
Deyson Venté <deiandresvent@hotmail.com>
2024-06-15 22:00:36 -0500

    Card para iterar en los registros, OK

--- src/app/Shorts/components/short-card/short-card.component.ts
+++ src/app/Shorts/components/short-card/short-card.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { IShorts } from '@interfaces';
 
 @Component({
   selector: 'app-short-card',
   standalone: true,
-  imports: [],
-  templateUrl: './short-card.component.html'
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIcon,
+    CommonModule,
+    RouterModule,
+  ],
+  templateUrl: './short-card.component.html',
 })
 export class ShortCardComponent {
-
+  short = input.required<IShorts>();
 }
