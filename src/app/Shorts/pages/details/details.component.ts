e2a85e0f7edf1eba43684323604b4ce6d3422af1
Deyson Venté <deiandresvent@hotmail.com>
2024-06-16 19:05:07 -0500

    vista para ver detalles del registro OK

--- src/app/Shorts/pages/details/details.component.ts
+++ src/app/Shorts/pages/details/details.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { IShorts } from '@interfaces';
+import { ShortsService } from '@services/shorts.service';
+import { SpinnerComponent } from '@shared/spinner/spinner.component';
+import { ShortTypePipe } from '../../../Pipes/short-type.pipe';
+import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './details.component.html'
+  imports: [
+    SpinnerComponent,
+    ShortTypePipe,
+    CommonModule,
+    MatIcon,
+    MatButton,
+    RouterModule,
+  ],
+  templateUrl: './details.component.html',
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private shortsService = inject(ShortsService);
+  private router = inject(Router);
+  private activateRoute = inject(ActivatedRoute);
+  mensaje = 'Cargando información del Short';
+  isLoading = true;
+  short?: IShorts;
 
+  ngOnInit() {
+    const id = this.activateRoute.snapshot.paramMap.get('id');
+    this.shortsService.getById(Number.parseInt(id!)).subscribe(
+      (resp) => {
+        if (resp.data) {
+          this.short = resp.data;
+          setTimeout(() => {
+            this.isLoading = false;
+          }, 1500);
+        }
+      },
+      (isError: HttpErrorResponse) => {
+        this.router.navigateByUrl('/shorts');
+      }
+    );
+  }
 }
