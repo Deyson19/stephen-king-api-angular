5354ccb2a45902967a9dcae5c3844c086ec59870
Deyson Venté <deiandresvent@hotmail.com>
2024-06-16 19:04:48 -0500

    Card-Actions mejorado, mat-card con sombra.

--- src/app/Shorts/components/short-card/short-card.component.ts
+++ src/app/Shorts/components/short-card/short-card.component.ts
@@ -1,22 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { IShorts } from '@interfaces';
 import { ShortTypePipe } from '../../../Pipes/short-type.pipe';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-short-card',
   standalone: true,
   imports: [
     MatCardModule,
-    MatButtonModule,
     MatIcon,
     CommonModule,
     RouterModule,
     ShortTypePipe,
+    MatButton,
   ],
   templateUrl: './short-card.component.html',
 })
