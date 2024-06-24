71e1e512bbc1e19f2f86bfb133221ef809bb1999
Deyson Venté <deiandresvent@hotmail.com>
2024-06-24 12:00:05 -0500

    vista padre para mostrar el listado de villanos.

--- src/app/Villains/pages/list/list.component.ts
+++ src/app/Villains/pages/list/list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { SpinnerComponent } from '@shared/spinner/spinner.component';
+import { VillainsTableComponent } from '../../components/villains-table/villains-table.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './list.component.html'
+  imports: [SpinnerComponent, VillainsTableComponent],
+  templateUrl: './list.component.html',
 })
-export class ListComponent {
+export class ListComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2800);
+  }
+  isLoading = true;
+  mensaje = 'Cargando listado de villanos';
 
+  tablaLista(x: boolean) {
+    if (x) {
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 1700);
+      return;
+    }
+
+    this.mensaje = 'La tabla no está lista o no hay registros para mostrar';
+    this.isLoading = true;
+  }
 }
